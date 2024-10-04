50540db4b3273ddf7f3828f10dd885a2c56c3226
Mohit Juneja <mohit0920@icloud.com>
2024-10-04 20:59:59 +0000

    src main added

---
+++ src/main/java/com/mohit/db_work/DbWorkApplication.java
@@ -0,0 +1,15 @@
+package com.mohit.db_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWorkApplication {
+
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbWorkApplication.class, args);
+	}
+
+}
