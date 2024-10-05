46a588aec84614d243da6a6dde4ec93485c7510a
Mohit Juneja <mohit0920@icloud.com>
2024-10-05 20:38:36 +0000

    Added hello

--- src/main/java/com/mohit/db_work/DbWorkApplication.java
+++ src/main/java/com/mohit/db_work/DbWorkApplication.java
@@ -2,11 +2,18 @@ package com.mohit.db_work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class DbWorkApplication {
 
-	
+	@GetMapping("/hello")
+	public String hello(){
+		return "hello from service.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbWorkApplication.class, args);
